cba25ed6b5e9537cdf700aa6b8e2094d1121ab78
felsa <felsacor@gmail.com>
2021-10-11 15:19:35 -0300

    Fix NaN bug in timer numbers, more responsive for mobile displays

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let eventDate = new Date(eventDateComponent.value);
 let currentDate = new Date();
 let timeUntilEvent;
 
+
 //clock numbers
 let clockDays    = document.querySelector("#days")   ;
 let clockHours   = document.querySelector("#hours")  ;
@@ -27,21 +28,22 @@ function updateEventDate () {
 
 function updateTimer(now, eventTime) {
     timeUntilEvent  = eventTime - now;
-    
-    let daysUntilEvent    =  timeUntilEvent / 8.64e7;
-    let hoursUntilEvent   = (timeUntilEvent % 8.64e7) / 3.6e6;
-    let minutesUntilEvent = (timeUntilEvent % 3.6e6 ) / 60_000;
-    let secondsuntilEvent = (timeUntilEvent % 60_000) / 1000;
+    if (timeUntilEvent < 0) {
+        clockDays.innerHTML    = "D";
+        clockHours.innerHTML   = "O";
+        clockMinutes.innerHTML = "N";
+        clockSeconds.innerHTML = "E";
+    } else {
+        let daysUntilEvent    =  timeUntilEvent / 8.64e7;
+        let hoursUntilEvent   = (timeUntilEvent % 8.64e7) / 3.6e6;
+        let minutesUntilEvent = (timeUntilEvent % 3.6e6 ) / 60_000;
+        let secondsuntilEvent = (timeUntilEvent % 60_000) / 1000;
 
-    clockDays.innerHTML    = Math.trunc(daysUntilEvent);
-    clockHours.innerHTML   = Math.trunc(hoursUntilEvent);
-    clockMinutes.innerHTML = Math.trunc(minutesUntilEvent);
-    clockSeconds.innerHTML = Math.trunc(secondsuntilEvent);
-    
-    clockDays.innerHTML = Math.trunc(timeUntilEvent / 8.64e7);
-    
-    console.log(timeUntilEvent);
-    
+        clockDays.innerHTML    = isNaN(daysUntilEvent)    ? "00" : Math.trunc(daysUntilEvent);
+        clockHours.innerHTML   = isNaN(hoursUntilEvent)   ? "00" : Math.trunc(hoursUntilEvent);
+        clockMinutes.innerHTML = isNaN(minutesUntilEvent) ? "00" : Math.trunc(minutesUntilEvent);
+        clockSeconds.innerHTML = isNaN(secondsuntilEvent) ? "00" : Math.trunc(secondsuntilEvent);
+    }
 }
 
 
